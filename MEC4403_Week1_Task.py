6d3ab8ac8e67ac805eea00b41217586a9ca745f2
Joel Brunton <q1021474@umail.usq.edu.au>
2025-05-29 21:18:43 +1000

    initial commit

---
+++ MEC4403_Week1_Task.py
@@ -0,0 +1,10 @@
+def PrintName():
+    name = input("Enter your name: ")
+    print('Your name is ' + name)
+
+def FavNumSq(i):
+    return i * i
+num = input("Enter your favourite number: ")
+
+print(FavNumSq(int(num)))
+
